6dcf93567fdcf7f2c623c2477dc736808a32c1c5
Kartik Namdev <137074717+Kartiknamdev@users.noreply.github.com>
2025-05-30 19:22:21 +0530

    Update constant.js for dev+prod

--- constant.js
+++ constant.js
@@ -1,2 +1,7 @@
-export const BACKEND_URL='http://localhost:3000/api/v1' // for local development in computer
+//export const BACKEND_URL='http://localhost:3000/api/v1' // for local development in computer
 // export const BACKEND_URL='https://freelancer-backend.vercel.app/api/v1' // for production deployment in vercel
+const isDevelopment = import.meta.env.MODE === 'development';
+
+export const BACKEND_URL = isDevelopment 
+  ? 'http://localhost:3000/api/v1'  // development
+  : 'https://pba-git-main-kartikk12na-gmailcoms-projects.vercel.app'; // production
