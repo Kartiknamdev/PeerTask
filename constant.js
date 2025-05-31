83abaedf8d30c41a2bf730ba7e7f787aa4d82601
Kartik Namdev <kartikk12na@gmail.com>
2025-05-31 14:30:48 +0530

    commit 31st may afternoon

--- constant.js
+++ constant.js
@@ -2,4 +2,4 @@ const isDevelopment = import.meta.env.MODE === 'development';
 
 export const BACKEND_URL = isDevelopment 
   ? 'http://localhost:3000/api/v1'  // development
-  : 'https://pba2-kartikk12na-gmailcoms-projects.vercel.app'; // production
+  : 'https://pba2-kartikk12na-gmailcoms-projects.vercel.app/api/v1'; // production
